b63d47cc29dbc09ccd96ce18aa6806bbc1eb5b14
jingbin <770413277@qq.com>
2019-11-13 18:27:27 +0800

    add ScrollableLayout test 有卡顿问题

--- App/app/src/main/java/com/example/jingbin/app/StickActivity.java
+++ App/app/src/main/java/com/example/jingbin/app/StickActivity.java
@@ -2,28 +2,63 @@ package com.example.jingbin.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.jingbin.app.stick.ScrollableLayout;
 
 import java.util.ArrayList;
 
-public class StickActivity extends AppCompatActivity {
+import me.jingbin.library.ByRecyclerView;
+import me.jingbin.library.adapter.BaseByViewHolder;
+import me.jingbin.library.adapter.BaseRecyclerAdapter;
 
-    private ArrayList<MainActivity.ViewPagerBean> viewList;
+/**
+ * @author jingbin
+ * https://github.com/w446108264/ScrollableLayout 滑动时有卡顿现象
+ */
+public class StickActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick);
-        initTabData();
-//        ListView viewById = findViewById(R.id.vp_scroll);
-//        viewById.addHeaderView(LayoutInflater.from(this).inflate(R.layout.item_goods_evaluation_empty, viewById, false));
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        arrayList.add("22222");
+        ByRecyclerView viewById = findViewById(R.id.vp_scroll);
+        ScrollableLayout scrollableLayout = findViewById(R.id.sl_root);
+        viewById.setAdapter(new InAdapter(R.layout.item_goods_evaluation_empty, arrayList));
+
+        scrollableLayout.getHelper().setCurrentScrollableContainer(viewById);
+
     }
 
+    private class InAdapter extends BaseRecyclerAdapter<String> {
+        public InAdapter(int layoutId, ArrayList<String> arrayList) {
+            super(layoutId, arrayList);
+        }
 
-    /**
-     * 图文详情 产品参数 用户评价
-     */
-    private void initTabData() {
-        viewList = new ArrayList<MainActivity.ViewPagerBean>();
+        @Override
+        protected void bindView(BaseByViewHolder<String> holder, String bean, int position) {
+            holder.setText(R.id.tv_evalu, bean);
+        }
     }
-
 }
